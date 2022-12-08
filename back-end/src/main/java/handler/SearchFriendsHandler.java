c6b97dd106970c331a44166d07b98a1282e5d29e
Jhon Raimund Aclan <jhonaclan@gmail.com>
2022-12-07 17:40:07 -0800

    Update SearchFriendsHandler.java
    update

--- back-end/src/main/java/handler/SearchFriendsHandler.java
+++ back-end/src/main/java/handler/SearchFriendsHandler.java
@@ -1,2 +1,32 @@
-package handler;public class SearchFriendsHandler {
+package handler;
+
+import dao.FriendsDao;
+import dto.FriendsDto;
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.Document;
+
+public class SearchFriendsHandler {
+
+    private final FriendsDao friendsDao;
+
+    public SearchFriendsHandler(FriendsDao friendsDao) {
+        this.friendsDao = friendsDao;
+    }
+
+    public List<String> search(String query) {
+        List<String> results = new ArrayList<>();
+
+        // Convert the query to lowercase to make the search case-insensitive
+        query = query.toLowerCase();
+
+        // Search the friends list for matches with the given query
+        for (FriendsDto friend : friendsDao.query(new Document())) {
+            if (friend.getFriendId().toLowerCase().contains(query)) {
+                results.add(friend.getFriendId());
+            }
+        }
+
+        return results;
+    }
 }
