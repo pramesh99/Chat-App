77d78e2a9e74a3079938956a2bc951c83aadcf8c
Jhon Raimund Aclan <jhonaclan@gmail.com>
2022-12-14 20:51:15 -0800

    update front end
    update front end

--- front-end/src/pages/SearchFriend.js
+++ front-end/src/pages/SearchFriend.js
@@ -1,28 +1,59 @@
 import React, { useState } from 'react';
+import Cookies from "universal-cookie";
 
 function SearchFriends() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [friendsList, setFriendsList] = useState([]);
-  const [filteredFriendsList, setFilteredFriendsList] = useState(friendsList);
-  const handleSearch = (event) => {
-    const searchQuery = event.target.value;
-    setSearchQuery(searchQuery);
+    const cookies = new Cookies();
 
-    // Filter the friends list to only include the given search query
-    const filteredFriendsList = friendsList.filter((friend) => friend.name.includes(searchQuery));
-    setFilteredFriendsList(filteredFriendsList);
-  };
+    const [prevSearchedFriends, setPrevSearchedFriends] = useState(cookies.get('prevSearchedFriends') || []);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [friendsList, setFriendsList] = useState([]);
+    const [filteredFriendsList, setFilteredFriendsList] = useState(friendsList);
 
-  return (
-    <div>
-      <input type="text" value={searchQuery} onChange={handleSearch} />
-      <ul>
-        {filteredFriendsList.map((friend) => (
-          <li key={friend.id}>{friend.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    // Retrieve the list of friends from the API
+    useEffect(() => {
+        fetch('/api/friends')
+            .then(res => res.json())
+            .then(friendsList => setFriendsList(friendsList))
+            .catch(err => console.log(err));
+    }, []);
+
+    const handleSearch = (event) => {
+        const searchQuery = event.target.value;
+        setSearchQuery(searchQuery);
+
+        // Filter the friends list to only include the given search query
+        const filteredFriendsList = friendsList.filter((friend) => friend.name.includes(searchQuery));
+        setFilteredFriendsList(filteredFriendsList);
+    };
+
+    // Show a list of previously searched friends
+    if (!prevSearchedFriends.includes(searchQuery) && searchQuery !== '') {
+        setPrevSearchedFriends([...prevSearchedFriends, searchQuery]);
+        cookies.set('prevSearchedFriends', [...prevSearchedFriends, searchQuery], { path: '/' });
+    };
+
+    return (
+        <div>
+            <input type="text" value={searchQuery} onChange={handleSearch} />
+            <ul>
+                {filteredFriendsList.length > 0 ? (
+                    filteredFriendsList.map((friend) => (
+                        <li key={friend.id}>{friend.name}</li>
+                    ))
+                ) : (
+                    <li>No matching friends found.</li>
+                )}
+            </ul>
+            <div>
+                <h3>Previously searched friends:</h3>
+                <ul>
+                    {prevSearchedFriends.map(friend => (
+                        <li key={friend}>{friend}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default SearchFriends;
